31e52ddfa573901cdbc9cd264c047b04629176c6
Satyam <satyam@satyam.com.ar>
2016-01-04 18:47:10 +0100

    minor cosmetic change

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const server = http.createServer()
   .on('request', (req, res) => {
     console.log(`Received request for ${req.url}`);
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.write('Hello World\n');
+    res.write('Hello World!\n');
     res.end(`Received request for ${req.url}`);
   })
   .listen(port, () => {
