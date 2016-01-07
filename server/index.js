7cb1ba1482f2043d0a6d7acb18edcef87e48a7b9
Satyam <satyam@satyam.com.ar>
2016-01-07 18:18:14 +0100

    npm start and environment options

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
 const http = require('http');
 
-const PORT = 8080;
+const PORT = process.env.npm_package_myServerApp_port || 8080;
 
 http.createServer((req, res) => {
-    console.log(`Received request for ${req.url}`);
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.write('Hello World!\n');
-    res.end(`Received request for ${req.url}`);
-  })
-  .listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}/`);
-  });
+  console.log(`Received request for ${req.url}`);
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.write('Hello World!\n');
+  res.end(`Received request for ${req.url}`);
+})
+.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
