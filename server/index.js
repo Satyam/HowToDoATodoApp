d890c1ebaeca2dd008a11b36ad059465d5f2b259
Satyam <satyam@satyam.com.ar>
2016-01-09 16:48:23 +0100

    Using Express routes

--- server/index.js
+++ server/index.js
@@ -4,14 +4,18 @@ const app = express();
 
 const PORT = process.env.npm_package_myServerApp_port || 8080;
 
+app.get('/hello', (req, res) => res.send('Hi, long time no see!'));
+
+app.get('/bye', (req, res) => res.send('See you later'));
+
 app.get('*', (req, res) => {
   console.log(`Received request for ${req.url}`);
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.write('Hello World!\n');
-  res.end(`Received request for ${req.url}`);
+  res.type('text')
+    .status(200)
+    .send(`Received request for ${req.url}`);
 });
 
 http.createServer(app)
-.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
-});
+  .listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}/`);
+  });
