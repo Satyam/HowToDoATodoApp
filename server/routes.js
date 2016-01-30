45f44169077c632df779c59fce67ec6da01975c9
Satyam <satyam@satyam.com.ar>
2016-01-30 18:34:37 +0100

    chapter 9-2: validating through middleware

--- server/routes.js
+++ server/routes.js
@@ -3,107 +3,115 @@
 const express = require('express');
 const projects = require('./projects.js');
 
-const processPrj = (op, res, keys, data, options) => {
-  projects[op](keys, data, options, (err, data) => {
+const send400 = res => void res.status(400).send('Bad request');
+
+const testFields = /^\s*\w+\s*(,\s*\w+\s*)*$/;
+const testSearch = /^\s*\w+\s*=\s*\w[\w\s]*$/;
+
+const add$valid = (req, res, next) => {
+  req.$valid = { };
+  next();
+};
+
+const validatePid = (req, res, next) => {
+  let pid = Number(req.params.pid);
+  if (Number.isNaN(pid)) return send400(res);
+  req.$valid.keys = {pid};
+  next();
+};
+
+const validateTid = (req, res, next) => {
+  let pid = Number(req.params.pid);
+  let tid = Number(req.params.tid);
+  if (Number.isNaN(pid) || Number.isNaN(tid)) return send400(res);
+  req.$valid.keys = {pid, tid};
+  next();
+};
+
+const validateOptions = (req, res, next) => {
+  let fields = req.query.fields;
+  let search = req.query.search;
+
+  if (
+    (fields && !testFields.test(fields)) ||
+    (search && !testSearch.test(search))
+  ) {
+    return send400(res);
+  }
+  req.$valid.options = {fields, search};
+  next();
+};
+
+const validatePrjData = (req, res, next) => {
+  let name = req.body.name;
+  let descr = req.body.descr;
+  if (name === undefined && descr === undefined) return send400(res);
+  let data = {};
+  if (name !== undefined) data.name = name;
+  if (descr !== undefined) data.descr = descr;
+  req.$valid.data = data;
+  next();
+};
+
+const validateTaskData = (req, res, next) => {
+  let descr = req.body.descr;
+  let complete = req.body.complete;
+  if (descr === undefined && complete === undefined) return send400(res);
+  let data = {};
+  if (descr !== undefined) data.descr = descr;
+  if (complete !== undefined) data.complete = !!complete;
+  req.$valid.data = data;
+  next();
+};
+
+const processPrj = (op, req, res) => {
+  const valid = req.$valid;
+  projects[op](valid.keys, valid.data, valid.options, (err, data) => {
     if (err) return void res.status(500).send(err);
     if (data === null) return void res.status(404).send('Item(s) not found');
     res.json(data);
   });
 };
 
-const send400 = res => void res.status(400).send('Bad request');
-
-const testFields = /^\s*\w+\s*(,\s*\w+\s*)*$/;
-const testSearch = /^\s*\w+\s*=\s*\w[\w\s]*$/;
-
 module.exports = (dataRouter, done) => {
   const projectRouter = express.Router();
-  dataRouter.use('/projects', projectRouter);
+  dataRouter.use('/projects', add$valid, projectRouter);
 
-  projectRouter.get('/', (req, res) => {
-    let fields = req.query.fields;
-    let search = req.query.search;
-
-    if (
-      (fields && !testFields.test(fields)) ||
-      (search && !testSearch.test(search))
-    ) {
-      return send400(res);
-    }
-    processPrj('getAllProjects', res, null, null, {fields, search});
+  projectRouter.get('/', validateOptions, (req, res) => {
+    processPrj('getAllProjects', req, res);
   });
 
-  projectRouter.get('/:pid', (req, res) => {
-    let pid = Number(req.params.pid);
-    if (Number.isNaN(pid)) return send400(res);
-    processPrj('getProjectById', res, {pid});
+  projectRouter.get('/:pid', validatePid, (req, res) => {
+    processPrj('getProjectById', req, res);
   });
 
-  projectRouter.get('/:pid/:tid', (req, res) => {
-    let pid = Number(req.params.pid);
-    let tid = Number(req.params.tid);
-    if (Number.isNaN(pid) || Number.isNaN(tid)) return send400(res);
-    processPrj('getTaskByTid', res, {pid, tid});
+  projectRouter.get('/:pid/:tid', validateTid, (req, res) => {
+    processPrj('getTaskByTid', req, res);
   });
 
-  projectRouter.post('/', (req, res) => {
-    let name = req.body.name;
-    let descr = req.body.descr;
-    if (name === undefined && descr === undefined) return send400(res);
-    let data = {};
-    if (name !== undefined) data.name = name;
-    if (descr !== undefined) data.descr = descr;
-    processPrj('addProject', res, null, data);
+  projectRouter.post('/', validatePrjData, (req, res) => {
+    processPrj('addProject', req, res);
   });
 
-  projectRouter.post('/:pid', (req, res) => {
-    let pid = Number(req.params.pid);
-    if (Number.isNaN(pid)) return send400(res);
-    let descr = req.body.descr;
-    let complete = req.body.complete;
-    if (descr === undefined && complete === undefined) return send400(res);
-    let data = {};
-    if (descr !== undefined) data.descr = descr;
-    if (complete !== undefined) data.complete = !!complete;
-    processPrj('addTaskToProject', res, {pid}, data);
+  projectRouter.post('/:pid', validatePid, validateTaskData, (req, res) => {
+    processPrj('addTaskToProject', req, res);
   });
 
-  projectRouter.put('/:pid', (req, res) => {
-    let pid = Number(req.params.pid);
-    if (Number.isNaN(pid)) return send400(res);
-    let name = req.body.name;
-    let descr = req.body.descr;
-    if (name === undefined && descr === undefined) return send400(res);
-    let data = {};
-    if (name !== undefined) data.name = name;
-    if (descr !== undefined) data.descr = descr;
-    processPrj('updateProject', res, {pid}, data);
+  projectRouter.put('/:pid', validatePid, validatePrjData, (req, res) => {
+    processPrj('updateProject', req, res);
   });
 
-  projectRouter.put('/:pid/:tid', (req, res) => {
-    let pid = Number(req.params.pid);
-    let tid = Number(req.params.tid);
-    if (Number.isNaN(pid) || Number.isNaN(tid)) return send400(res);
-    let descr = req.body.descr;
-    let complete = req.body.complete;
-    if (descr === undefined && complete === undefined) return send400(res);
-    let data = {};
-    if (descr !== undefined) data.descr = descr;
-    if (complete !== undefined) data.complete = !!complete;
-    processPrj('updateTask', res, {pid, tid}, data);
+  projectRouter.put('/:pid/:tid', validateTid, validateTaskData, (req, res) => {
+    processPrj('updateTask', req, res);
   });
 
-  projectRouter.delete('/:pid', (req, res) => {
-    let pid = Number(req.params.pid);
-    if (Number.isNaN(pid)) return send400(res);
-    processPrj('deleteProject', res, {pid});
+  projectRouter.delete('/:pid', validatePid, (req, res) => {
+    processPrj('deleteProject', req, res);
   });
 
-  projectRouter.delete('/:pid/:tid', (req, res) => {
-    let pid = Number(req.params.pid);
-    let tid = Number(req.params.tid);
-    if (Number.isNaN(pid) || Number.isNaN(tid)) return send400(res);
-    processPrj('deleteTask', res, {pid, tid});
+  projectRouter.delete('/:pid/:tid', validateTid, (req, res) => {
+    processPrj('deleteTask', req, res);
   });
+
   projects.init(done);
 };
