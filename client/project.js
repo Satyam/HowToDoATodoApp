73ac4238a3a678ec5667f446b6b60add8e60e1e3
Satyam <satyam@satyam.com.ar>
2016-02-02 15:18:32 +0100

    chapter 10-3: transpiling

---
+++ client/project.js
@@ -0,0 +1,16 @@
+/* globals axios:false */
+'use strict';
+axios.get(`/data/v1/projects/${window.location.search.substr(1)}`)
+  .then(response => {
+    let prj = response.data;
+    document.getElementById('contents').innerHTML =
+      `<h1>${prj.name}</h1><p>${prj.descr}</p><ul>${
+        Object.keys(prj.tasks).reduce((prev, tid) => {
+          let task = prj.tasks[tid];
+          return prev + `<li><input type="checkbox" ${
+            task.complete ? 'checked' : ''
+          } /> &nbsp; ${task.descr}</li>`;
+        }, '')
+      }</ul>`;
+    document.title = `Project ${prj.pid}: ${prj.name}`;
+  });
