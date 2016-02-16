3603caed5859b71a500e28045486e377603a0705
Satyam <satyam@satyam.com.ar>
2016-02-16 23:03:15 +0100

    Added syntetic event to component

--- client/project.js
+++ client/project.js
@@ -1,11 +1,19 @@
 import React from 'react';
 const data = require('./data.js');
 
-const Task = ({ task }) => (
-  <li>
-    <input type="checkbox" defaultChecked={task.complete} /> &nbsp; {task.descr}
-  </li>
-);
+const Task = ({ task, tid, onClick }) => {
+  const handler = (typeof onClick === 'function') && (ev => {
+    if (ev.button || ev.shiftKey || ev.altKey || ev.metaKey || ev.ctrlKey) return;
+    ev.preventDefault();
+    onClick({ tid });
+  });
+
+  return (
+    <li onClick={handler}>
+      <input type="checkbox" defaultChecked={task.complete} /> &nbsp; {task.descr}
+    </li>
+  );
+};
 
 Task.propTypes = {
   task: React.PropTypes.shape({
@@ -14,13 +22,16 @@ Task.propTypes = {
   }),
 };
 
-const TaskList = ({ tasks }) => (
-  <ul className="task-list">{
-    Object.keys(tasks).map(tid => (
-      <Task key={tid} task={tasks[tid]} />
-    ))
-  }</ul>
-);
+const TaskList = ({ tasks }) => {
+  const handler = ev => console.log('click', ev);
+  return (
+    <ul className="task-list">{
+      Object.keys(tasks).map(tid => (
+        <Task key={tid} task={tasks[tid]} tid={tid} onClick={handler}/>
+      ))
+    }</ul>
+  );
+};
 
 TaskList.propTypes = {
   tasks: React.PropTypes.object,
