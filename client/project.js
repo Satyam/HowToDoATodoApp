3b6439f589c9fdc7fa37ebd8497e61f52e684fa7
Satyam <satyam@satyam.com.ar>
2016-02-18 15:16:45 +0100

    chapter 13-2: handling changes and triggering re-render

--- client/project.js
+++ client/project.js
@@ -1,39 +1,55 @@
 import React from 'react';
 const data = require('./data.js');
 
-const Task = ({ task, tid, onClick }) => {
+const Task = ({ descr, complete, tid, onClick }) => {
   const handler = (typeof onClick === 'function') && (ev => {
     if (ev.button || ev.shiftKey || ev.altKey || ev.metaKey || ev.ctrlKey) return;
-    ev.preventDefault();
+    /* See: https://facebook.github.io/react/docs/forms.html#potential-issues-with-checkboxes-and-radio-buttons
+    */
+    // ev.preventDefault();
     onClick({ tid });
   });
 
   return (
     <li onClick={handler}>
-      <input type="checkbox" defaultChecked={task.complete} /> &nbsp; {task.descr}
+      <input type="checkbox" readOnly checked={complete} /> &nbsp; {descr}
     </li>
   );
 };
 
 Task.propTypes = {
-  task: React.PropTypes.shape({
-    complete: React.PropTypes.bool,
-    descr: React.PropTypes.string,
-  }),
+  complete: React.PropTypes.bool,
+  descr: React.PropTypes.string,
   tid: React.PropTypes.string,
   onClick: React.PropTypes.func,
 };
 
-const TaskList = ({ tasks }) => {
-  const handler = ev => console.log('click', ev);
-  return (
-    <ul className="task-list">{
-      Object.keys(tasks).map(tid => (
-        <Task key={tid} task={tasks[tid]} tid={tid} onClick={handler}/>
-      ))
-    }</ul>
-  );
-};
+class TaskList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = props.tasks;
+    this._handler = this.handler.bind(this);
+  }
+  handler({ tid }) {
+    const task = this.state[tid];
+    this.setState({ [tid]: Object.assign(task, { complete: !task.complete }) });
+  }
+  render() {
+    const tasks = this.state;
+    return (
+      <ul className="task-list">{
+        Object.keys(tasks).map(tid => (
+          <Task key={tid}
+            descr={tasks[tid].descr}
+            complete={tasks[tid].complete}
+            tid={tid}
+            onClick={this._handler}
+          />
+        ))
+      }</ul>
+    );
+  }
+}
 
 TaskList.propTypes = {
   tasks: React.PropTypes.object,
