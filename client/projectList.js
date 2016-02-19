cef745ba04d24d9a98639010f1f006b7978a10e9
Satyam <satyam@satyam.com.ar>
2016-02-19 19:00:00 +0100

    Chapter 13-5: adding a pending task count to each project

--- client/projectList.js
+++ client/projectList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router';
 const data = require('./data.js');
 
-const PrjItem = ({ pid, name, active }) => (
+const PrjItem = ({ pid, name, active, pending }) => (
   <li className={active ? 'selected' : ''}>
     {
       active
@@ -10,13 +10,15 @@ const PrjItem = ({ pid, name, active }) => (
       : (<Link to={`/project/${pid}`}>
           {name}
         </Link>)
-    }
+    } [Pending: {pending}]
   </li>
 );
 
 PrjItem.propTypes = {
   pid: React.PropTypes.string.isRequired,
   name: React.PropTypes.string.isRequired,
+  active: React.PropTypes.bool.isRequired,
+  count: React.PropTypes.number.isRequired,
 };
 
 const ProjectList = ({ children, params: { pid: activePid } }) => (
@@ -28,6 +30,12 @@ const ProjectList = ({ children, params: { pid: activePid } }) => (
           active={activePid === pid}
           pid={pid}
           name={data[pid].name}
+          pending={
+            Object.keys(data[pid].tasks).reduce(
+              (count, tid) => data[pid].tasks[tid].complete ? count : count + 1,
+              0
+            )
+          }
         />)
       )
     }</ul>
