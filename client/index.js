a02488245835cd2bd231a288ec75910cd47e68ea
Satyam <satyam@satyam.com.ar>
2016-02-29 18:29:27 +0100

    Added debugging and performance analyzer

--- client/index.js
+++ client/index.js
@@ -3,10 +3,17 @@ import { render } from 'react-dom';
 import { Router, Route } from 'react-router';
 import createBrowserHistory from 'history/lib/createBrowserHistory';
 
+if (process.env.NODE_ENV !== 'production') {
+  window.Perf = require('react-addons-perf');
+}
+
 import { createStore } from 'redux';
 import reducers from './reducers';
 
-const store = createStore(reducers);
+const store = createStore(
+  reducers,
+  window.devToolsExtension ? window.devToolsExtension() : undefined
+);
 
 import { Provider } from 'react-redux';
 
