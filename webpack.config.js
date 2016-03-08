bbc66cca40306e172cb84b207a315ac5481afa4c
Satyam <satyam@satyam.com.ar>
2016-03-08 16:19:31 +0100

    npm package updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
+var path = require('path');
+
 module.exports = {
   entry: './client',
   output: {
-    path: __dirname + '/public/lib',
+    path: path.join(__dirname, '/public/lib'),
     filename: 'bundle.js'
   },
   module: {
