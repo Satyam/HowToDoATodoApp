eea731e18ec367daf54741fd2605ec48a1e2c51b
Satyam <satyam@satyam.com.ar>
2016-04-17 17:47:14 +0200

    Gogin back on cancel didn't work when loaded directly

--- client/components/editProject.js
+++ client/components/editProject.js
@@ -92,7 +92,7 @@ EditProject.propTypes = {
   cancelButton: PropTypes.func,
 };
 
-import { getProjectById, addProject, updateProject, push, goBack } from 'client/actions';
+import { getProjectById, addProject, updateProject, push, replace } from 'client/actions';
 
 EditProject.serverInit = (dispatch, { params }) => dispatch(getProjectById(params.pid));
 
@@ -116,7 +116,7 @@ export const mapDispatchToProps = (dispatch, { params }) => ({
       .then(response => dispatch(push(`/project/${response.data.pid}`)));
   },
   cancelButton: ev => {
-    if (isPlainClick(ev)) dispatch(goBack());
+    if (isPlainClick(ev)) dispatch(replace(`/project/${params.pid}`));
   },
 });
 
